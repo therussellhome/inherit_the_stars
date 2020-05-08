12df15376edd0359d76cc91a3a5c29125702b488
Chris Russell <github@therussellhome.us>
2020-05-07 21:03:28 -0400

    merge everything

--- host/_test.py
+++ host/_test.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-import game_engine
 import planet
-import player
-import race
+import sun
 import star_system
+import player
+import game_engine
 
 print('test.py - start')
 planet._test()
+sun._test()
 star_system._test()
 player._test()
 game_engine._test()
--- host/sun.py
+++ host/sun.py
@@ -1,8 +1,19 @@
-import planet
+from planet import Planet
+import game_engine
 
-class sun(planet.Planet):
-    def __init__(self):
-        print(self)
-        pass
-    
-s = sun()
+""" Default values (default, min, max)  """
+__defaults = {
+    'distance': [0, 0, 0]
+}
+
+""" TODO """
+class Sun(Planet):
+    def __init__(self, **kwargs):
+        super()._apply_defaults(**kwargs)
+
+# Register the class with the game engine
+game_engine.register(Sun, defaults=__defaults)
+
+""" TODO """
+def _test():
+    pass
