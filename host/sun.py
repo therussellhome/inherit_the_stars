997fc8105b44283f429080fa850573e6159c5899
Tiernan Russell <tiernan@therussellhome.us>
2020-05-08 16:14:07 -0400

    so that i can pull from origin master

--- host/sun.py
+++ host/sun.py
@@ -1,8 +1,17 @@
-import planet
+from planet import Planet
 
-class sun(planet.Planet):
-    def __init__(self):
-        print(self)
-        pass
-    
-s = sun()
+class sun(Planet):
+    pass
+
+def test_expect(actual, expect, test_id):
+    if expect != actual:
+        print('ERROR ', test_id, ' got ', actual, ' expected ', expect)
+
+def test():
+    print('sun.test-start')
+    s = sun()
+    test_expect(s.gravity, 50, 'sun_test #1')
+    test_expect(s.tempiter, 50, 'sun_test #1')
+    test_expect(s.radeation, 50, 'sun_test #1')
+    test_expect(s.player, None, 'sun_test #1')
+    print('sun.test-end')
--- host/test.py
+++ host/test.py
@@ -4,9 +4,11 @@ import game_engine
 import planet
 import player
 import race
+import sun
 
 print('test.py - start')
 planet._test()
+sun.test()
 player._test()
 game_engine._test()
 print('test.py - stop')
