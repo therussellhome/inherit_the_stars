78967acc46ccb6c67c031dc1be7f0603b3b934c6
Chris Russell <github@therussellhome.us>
2020-05-25 17:24:20 -0400

    fix unit tests

--- stars/intel.py
+++ stars/intel.py
@@ -2,7 +2,7 @@ from . import game_engine
 from . import reference
 
 """ Base class for the players view of the galaxy """
-class PlayerReport(game_engine.BaseClass):
+class Intel(game_engine.BaseClass):
     def __init__(self, reference, **kwargs):
         self.__dict__.update(kwargs)
         if 'date' not in kwargs:
