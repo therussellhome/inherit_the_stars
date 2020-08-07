69231c4576f61ef4c51b56bab2b8ea083aaeb844
Aryon Russell <aryon@therussellhome.us>
2020-08-07 08:20:11 -0400

    started creating the actions from the ships perspective.

--- stars/ship.py
+++ stars/ship.py
@@ -14,8 +14,10 @@ __defaults = {
     'battle_plan': [BattlePlan()],
     'initative': [0, 0, sys.maxsize],
     'armor': [10, 0, sys.maxsize],
-    'shealds': [0, 0, sys.maxsize],
+    'sheaulds': [0, 0, sys.maxsize],
     'max_distance': [0, 0, sys.maxsize],
+    'damage_points': [0, 0, sys.maxsize],
+    'repair_points': [0, 0, sys.maxsize],
 }
 
 
@@ -59,6 +61,32 @@ class Ship(ShipDesign):
         self.location.z = z
         return fuel
     
+    def colonize(self, player, planet):
+        planet.colonize(player, copy.copy(player.colonize_minister), self.cargo, self.num_col_modules, self.num_col_modules, self.num_col_modules)
+    
+    def scan(self, player):
+        pass
+    
+    def lay_mines(self, player, system):
+        pass
+    
+    def open_repair_bays(self):
+        return self.repair_bay_repair_points
+    
+    def deploy_hyper_denial(self, player):
+        pass
+    
+    def scrap(self, location):
+        #scrap algorithem
+        #t = self.cost.titatium * scrap_factor
+        #l = self.cost.lithium * scrap_factor
+        #s = self.cost.silicon * scrap_factor
+        Cargo = Cargo(titanium = t, lithium = l, silicon = s, cargo_makx = (t + l + s))
+        if location not in game_engine.get('Planet/'):
+            game_engine.create_salvage(copy.copy(location), Cargo)
+        else:
+            location.on_surface += cargo + Cargo
+    
     """ Mines the planet if it is not colonized """
     def orbital_mining(self, planet):
         if planet.colonized == False:
@@ -74,10 +102,12 @@ class Ship(ShipDesign):
         return planet
     
     """ Repairs the ship if it needs it """
-    def repair(self, ship):
-        if ship.damage_points > 0:
-            ship.damage_points -= self.repair_points
-        return ship
+    def repair(self, amount):
+        if self.damage_points > 0:
+            self.damage_points -= amount
+    
+    def self_repair(self):
+        self.repair(self.repair_points)
     
     """ Bombs the planet if the planet is colonized """
     def bomb(self, p):
