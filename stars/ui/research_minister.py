3192ca77af2c5a1580dc192d11f46fb17aba2cd1
Ilyanna Russell <ilyanna@therussellhome.us>
2020-10-23 11:45:42 -0400

    test research minister and some of research minister

--- stars/ui/research_minister.py
+++ stars/ui/research_minister.py
@@ -24,26 +24,33 @@ __defaults = {
 class ResearchMinister(Player):
     """ Interact with UI """
     def _post(self, action, me):
-        self.research_current_energy_tech_level = max(research_current_energy_tech_level, 0)  
-        self.research_current_weapons_tech_level = max(research_current_weapons_tech_level, 0)
-        self.research_current_propulsion_tech_level = max(research_current_propulsion_tech_level, 0)
-        self.research_current_construction_tech_level = max(research_current_construction_tech_level, 0)
-        self.research_current_biotechnology_tech_level = max(research_current_biotechnology_tech_level, 0)
-        self.research_queue_item_1 = '' 
-        self.research_queue_item_2 = ''
-        self.research_queue_item_3 = ''
-        self.research_queue_item_4 = ''
-        self.research_queue_item_5 = ''
-        self.research_queue_item_6 = ''
-        self.research_queue_item_7 = ''
-        self.research_queue_item_8 = ''
-    def calc_cost(self):
-        energy_cost = research_modifier_energy * ((player.energy_tech_level ** 3) * 8 + 150)
-        weapons_cost = research_modifier_weapons * ((player.weapons_tech_level ** 3) * 8 + 150) 
-        propulsion_cost = research_modifier_propulsion * ((player.propulsion_tech_level ** 3) * 8 + 150)
-        construction_cost = research_modifier_construction * ((player.construction_tech_level ** 3) * 8 + 150) 
-        electronics_cost = research_modifier_electronics * ((player.electronics_tech_level ** 3) * 8 + 150)
-        biotech_cost = research_modifier_biotechnology * ((player.biotechnology_tech_level ** 3) * 8 + 150) 
+        self.research_current_energy_tech_level = max(min(research_current_energy_tech_level, 25), 0)  
+        self.research_current_weapons_tech_level = max(min(research_current_weapons_tech_level, 25), 0)
+        self.research_current_propulsion_tech_level = max(min(research_current_propulsion_tech_level, 25), 0)
+        self.research_current_construction_tech_level = max(min(research_current_construction_tech_level, 25), 0)
+        self.research_current_electronics_tech_level = max(min(research_current_electronics_tech_level, 25), 0)
+        self.research_current_biotechnology_tech_level = max(min(research_current_biotechnology_tech_level, 25), 0)
+        self.research_queue_item_1 = me.research_queue[0] 
+        self.research_queue_item_2 = me.research_queue[1] 
+        self.research_queue_item_3 = me.research_queue[2] 
+        self.research_queue_item_4 = me.research_queue[3] 
+        self.research_queue_item_5 = me.research_queue[4] 
+        self.research_queue_item_6 = me.research_queue[5] 
+        self.research_queue_item_7 = me.research_queue[6] 
+        self.research_queue_item_8 = me.research_queue[7] 
+    def calc_cost(self, field):
+        if field == 'energy':
+            return round(research_modifier_energy * ((player.energy_tech_level ** 3) * 8 + 150))
+        if field == 'weapons':
+            return round(research_modifier_weapons * ((player.weapons_tech_level ** 3) * 8 + 150)) 
+        if field == 'propulsion':
+            return round(research_modifier_propulsion * ((player.propulsion_tech_level ** 3) * 8 + 150))
+        if field == 'construction':
+            return round(research_modifier_construction * ((player.construction_tech_level ** 3) * 8 + 150)) 
+        if field == 'electronics':
+            return round(research_modifier_electronics * ((player.electronics_tech_level ** 3) * 8 + 150))
+        if field == 'biotechnology':
+            return round(research_modifier_biotechnology * ((player.biotechnology_tech_level ** 3) * 8 + 150))
     def research_bombs(self):
         pass
     def research_cloaks_and_ecm(self):
@@ -57,7 +64,7 @@ class ResearchMinister(Player):
     def research_hulls(self):
         pass
     def research_mech(self):
-        pass
+        pass 
     def research_orbital(self):
         pass
     def research_planetary(self):
