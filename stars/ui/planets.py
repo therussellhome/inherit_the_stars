6c1d1b6a500bc929a7a69911bfea9c755dad9047
Ilyanna Russell <ilyanna@therussellhome.us>
2020-11-06 15:53:11 -0500

    some of planet reports

--- stars/ui/planets.py
+++ stars/ui/planets.py
@@ -11,6 +11,13 @@ class Planets(Player):
     """ Interact with UI """
     def _post(self, action, me):
         pass
-
+    def p(self)
+        planets = []
+        for p in self.player.get_intel('Planet'):
+            planets.append(p)
+        for s in self.player.get_intel('Sun')
+            planet.append(s)
+        for p in planets:
+            self.planets_report.append('<td>' + p.get('name') + '</td><td>100,000</td>')
 
 Planets.set_defaults(Planets, __defaults, no_reset=[])
