d349a6ae22aeb3811feaab852a0b9e12bb38b6b9
Ilyanna Russell <ilyanna@therussellhome.us>
2020-11-25 10:23:05 -0500

    debugging Research Minister

--- stars/ui/research_minister.py
+++ stars/ui/research_minister.py
@@ -36,7 +36,7 @@ class ResearchMinister(PlayerUI):
         for t in self.player.research_queue:
             link = t.name.replace('\'', '\\\'').replace('\"', '\\\"')
             research_queue.append(t.name)
-            self.research_queue.append('<td class="hfill"><div class="tech tech_template">' + link + '</div></td>' \
+            self.research_queue.append('<td class="hfill"><div class="tech tech_template">' + t.name + '</div></td>' \
                     + '<td><i class="button far fa-trash-alt" title="Add to queue" onclick="post(\'research_minister\', \'?del=' + link + '\')"></i></td>')
         # Sort tech
         research_tech = []
@@ -59,7 +59,7 @@ class ResearchMinister(PlayerUI):
                 cost = t.level.calc_cost(self.player.race, self.player.tech_level, self.player.research_partial)
                 if cost > 0 and t.name not in research_queue: 
                     link = t.name.replace('\'', '\\\'').replace('\"', '\\\"')
-                    row = '<td class="hfill"><div class="tech tech_template">' + link + '</div></td>' \
+                    row = '<td class="hfill"><div class="tech tech_template">' + t.name + '</div></td>' \
                         + '<td><i class="button fas fa-cart-plus" title="Add to queue" onclick="post(\'research_minister\', \'?add=' + link + '\')"></i></td>'
                     research_tech.append((cost, row))
         research_tech.sort(key = lambda x: x[0])
