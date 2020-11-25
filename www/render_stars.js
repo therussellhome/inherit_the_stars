dc70ee82853ce21189c265d798248fcbf6f4a9a7
Chris Russell <github@therussellhome.us>
2020-11-24 23:11:51 -0500

    zoom and flyto work

--- www/render_stars.js
+++ www/render_stars.js
@@ -2,7 +2,15 @@ import * as THREE from '/three.module.js';
 
 const TERAMETER = 0.0001057;
 
-var scene, renderer, camera, look_at, camera_range, camera_phi, camera_theta, fixed_camera;
+// the systems group is added to the scene
+// each system gets a group added to systems
+// suns are children of the named system
+// -- special processing to NOT hide suns
+// planets and in-system objects are children of the named system
+// -- these are hidden unless the system is selected
+// intersteller objects are added directly to the scene
+
+var scene, renderer, camera, selected, camera_lookat, camera_flyto, fixed_camera, systems;
 
 init();
 
@@ -13,13 +21,15 @@ function init() {
     scene.fog = new THREE.FogExp2(0x000000, 0.001);
     renderer = new THREE.WebGLRenderer();
     div.appendChild( renderer.domElement );
+    // create systems group
+    systems = new THREE.Group();
+    scene.add(systems);
     // look at center of galaxy
-    look_at = null;
+    selected = systems;
     fixed_camera = false;
     camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, TERAMETER / 1000, 2000);
-    camera_range = 500;
-    camera_phi = 0;
-    camera_theta = 0;
+    camera_lookat = new THREE.Vector3();
+    camera_flyto = new THREE.Vector3(0, 0, 500);
     scene.add(camera);
     onWindowResize();
     // attach event listeners
@@ -27,26 +37,30 @@ function init() {
     div.addEventListener('submit', onSubmit);
     div.addEventListener('keydown', onKeyPress);
     div.addEventListener('click', onClick);
+    div.addEventListener('dblclick', onDoubleClick);
     div.addEventListener('wheel', onWheel);
 }
 
-function addToScene(name, x, y, z, size, color, alphaMap) {
-    var geometry = new THREE.BufferGeometry();
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute([0, 0, 0], 3));
-    var material = new THREE.PointsMaterial( { 
-        color: new THREE.Color(color),
-        alphaMap: alphaMap,
-        size: size,
-        sizeAttenuation: true,
-        transparent: true,
-        alphaTest: 0.5
-    } );
-    var p = new THREE.Points(geometry, material);
-    p.name = name;
-    p.position.set(x, y, z);
-    scene.add(p);
-    if(look_at == null) {
-        look_at = p;
+// Find a system object by name
+function findSystem(name) {
+    name = "System/" + name;
+    for(var i = 0; i < systems.children.length; i++) {
+        if(systems.children[i].name == name) {
+            return systems.children[i];
+        }
+    }
+    var system = new THREE.Group();
+    system.name = name;
+    systems.add(system);
+    return system;
+}
+
+// Add a point to the correct system or to the scene if intersteller
+function addPoint(point, system_name) {
+    if(system_name == "") {
+        scene.add(point);
+    } else {
+        findSystem(system_name).add(point);
     }
 }
 
@@ -55,75 +69,104 @@ function onSubmit() {
     if(json_map.hasOwnProperty('render_stars')) {
         if(json_map['render_stars'].hasOwnProperty('suns')) {
             if(json_map['render_stars']['suns'].length > 0) {
+                // load materials
+                var sun_alphaMap = new THREE.TextureLoader().load( "/particle.png" )
+                var sun_texture = new THREE.TextureLoader().load( "/particle.png" )
+                var planet_alphaMap = new THREE.TextureLoader().load( "/particle.png" )
+                var planet_texture = new THREE.TextureLoader().load( "/particle.png" )
                 var suns = json_map['render_stars']['suns'];
                 var planets = json_map['render_stars']['planets'];
-
-                var sphere = new THREE.TextureLoader().load( "/particle.png" )
-                var ring = new THREE.TextureLoader().load( "/particle-ring.png" )
-
-                for(var i=0; i<suns.length; i++) {
-//                    addToScene("System/" + suns[i].name, suns[i].x, suns[i].y, suns[i].z, 10, "#ffffff", sphere);
+                // reusable geometry
+                var geometry = new THREE.BufferGeometry();
+                geometry.setAttribute('position', new THREE.Float32BufferAttribute([0, 0, 0], 3));
+                // Add suns
+                for(var i = 0; i < suns.length; i++) {
+                    var material = new THREE.PointsMaterial( { 
+                        color: new THREE.Color(suns[i].color),
+                        map: sun_texture,
+                        alphaMap: sun_alphaMap,
+                        size: (suns[i].size + 200) * TERAMETER / 1000,
+                        sizeAttenuation: true,
+                        transparent: true,
+                        alphaTest: 0.5
+                    } );
+                    var system_name = suns[i].name.replaceAll(/\'.*/g, ""); // Eventually this should come from the python side
+                    var point = new THREE.Points(geometry, material);
+                    point.position.set(suns[i].x, suns[i].y, suns[i].z);
+                    point.name = "Sun/" + suns[i].name;
+                    addPoint(point, system_name);
                 }
-
-                for(var i=0; i<suns.length; i++) {
-                    var size = (suns[i].size + 200) * TERAMETER / 1000;
-                    addToScene("Sun/" + suns[i].name, suns[i].x, suns[i].y, suns[i].z, size, suns[i].color, sphere);
+                // Make dummy planets
+                var planet_colors = ["#990000", "#009900", "#000099", "#999999"];
+                for(var i = 0; i < suns.length; i++) {
+                    for(var j = 0; j < planet_colors.length; j++) {
+                        var material = new THREE.PointsMaterial( { 
+                            color: new THREE.Color(planet_colors[j]),
+                            map: planet_texture,
+                            alphaMap: planet_alphaMap,
+                            size: (Math.random() * 100 + 200) * TERAMETER / 10000,
+                            sizeAttenuation: true,
+                            transparent: true,
+                            alphaTest: 0.5
+                        } );
+                        var system_name = suns[i].name.replaceAll(/\'.*/g, ""); // Eventually this should come from the python side
+                        var point = new THREE.Points(geometry, material);
+                        point.position.set(suns[i].x + TERAMETER * Math.random() * 2 - TERAMETER, suns[i].y + TERAMETER * Math.random() * 2 - TERAMETER, suns[i].z);
+                        point.name = "Planet/" + system_name + " " + j;
+                        point.visible = false;
+                        addPoint(point, system_name);
+                    }
                 }
-
-                for(var i=0; i<suns.length; i++) {
-                    var size = (suns[i].size + 200) * TERAMETER / 10000;
-                    addToScene("Planet/" + suns[i].name, suns[i].x + TERAMETER, suns[i].y, suns[i].z, size, "#990000", sphere);
-                    addToScene("Planet/" + suns[i].name, suns[i].x - TERAMETER, suns[i].y, suns[i].z, size, "#009900", sphere);
-                    addToScene("Planet/" + suns[i].name, suns[i].x, suns[i].y + TERAMETER, suns[i].z, size, "#000099", sphere);
-                    addToScene("Planet/" + suns[i].name, suns[i].x, suns[i].y - TERAMETER, suns[i].z, size, "#999999", sphere);
-                }
-
-                for(var i=0; i<planets.length; i++) {
-//                    var size = (planets[i].size + 200) / 10 * TERAMETER;
-//                    addToScene("Planet/" + planets[i].name, planets[i].x, planets[i].y, planets[i].z, size, planets[i].color, sphere);
-                }
+                // Zoom to home world
+                select_object(systems.children[0].children[0], true); // Replace with with the home system's sun
+                // Render
                 window.setTimeout(render, 1000);
             }
         }
     }
 }
 
+// Zoom
 function onWheel(event) {
     event.preventDefault();
-    // zoom in
+    var offset = camera_flyto.clone().sub(selected.position);
+    var distance = camera_flyto.distanceTo(selected.position);
+    // zoom out
     if(event.deltaY > 0) {
-        camera_range *= 1.2;
+        offset.setLength(distance * 1.2);
+    // zoom in
     } else {
-        camera_range /= 1.2;
+        offset.setLength(Math.max(distance / 1.2, TERAMETER / 10));
     }
-    camera_range = Math.max(camera_range, TERAMETER / 10);
+    camera_flyto = selected.position.clone().add(offset);
     window.requestAnimationFrame(render);
 }
 
+// Key controls
 function onKeyPress(event) {
     console.log(event.keyCode);
     // up
     if(event.keyCode == 38) {
-        camera_phi += 0.01;
+        camera_offset.phi += 0.01;
     // down
     } else if(event.keyCode == 40) {
-        camera_phi -= 0.01;
+        camera_offset.phi -= 0.01;
     // left
     } else if(event.keyCode == 37) {
-        camera_theta -= 0.01;
+        camera_offset.theta -= 0.01;
     // right
     } else if(event.keyCode == 39) {
-        camera_theta += 0.01;
+        camera_offset.theta += 0.01;
     // reset
     } else if(event.key == "r") {
-        console.log('reset');
-        camera_range = TERAMETER * 2;
-        camera_phi = 0;
-        camera_theta = Math.PI;
+        camera_offset.radius = TERAMETER * 2;
+        camera_offset.phi = Math.PI / 2;
+        camera_offset.theta = Math.PI;
     }
     window.requestAnimationFrame(render);
 }
 
+// Click
 function onClick(event) {
     var mouse = new THREE.Vector2(
         ( event.clientX / window.innerWidth ) * 2 - 1,
@@ -131,23 +174,70 @@ function onClick(event) {
     );
     var raycaster = new THREE.Raycaster();
     raycaster.setFromCamera(mouse, camera);
-    var intersects = raycaster.intersectObjects(scene.children);
+    raycaster.params.Points.threshold = TERAMETER / 10;
+    raycaster.near = 0;
+    raycaster.far = 10;
+    var intersects = raycaster.intersectObjects(scene.children, true);
     if(intersects.length > 0) {
-        console.log(intersects[0].object.name);
-        //console.log(intersects[0].object);
-        look_at = intersects[0].object;
-//        look_at.material.opacity = 0;
-//        look_at.material.size = look_at.material.size / 100;
-        var relative = camera.position.clone().sub(look_at.position);
-        var spherical = new THREE.Spherical();
-        spherical.setFromCartesianCoords(relative.x, relative.y, relative.z);
-        camera_range = spherical.radius;
-        camera_phi = spherical.phi;
-        camera_theta = spherical.theta;
+        var d1 = intersects[0].object.position.distanceTo(intersects[0].point);
+        var best = 0;
+        for(var i=1; i<intersects.length; i++) {
+            var d2 = intersects[i].object.position.distanceTo(intersects[i].point);
+            if(d2 < d1) {
+                d1 = d2;
+                best = i;
+            }
+        }
+        select_object(intersects[best].object, false);
+    } else {
+        raycaster.params.Points.threshold = 10;
+        raycaster.near = 10;
+        raycaster.far = 10000;
+        intersects = raycaster.intersectObjects(systems.children, true);
+        console.log(intersects.length);
+        if(intersects.length > 0) {
+            select_object(intersects[0].object.parent.children[0], false);
+        }
     }
     window.requestAnimationFrame(render);
 }
 
+// Refocus on the clicked object
+function select_object(obj, flyto) {
+    console.log(obj.name, obj.parent.name);
+    system_visibility(selected, false);
+    selected = obj;
+    system_visibility(selected, true);
+    if(flyto) {
+        var z_offset = TERAMETER;
+        if(camera.position.z < selected.position.z) {
+            z_offset = - TERAMETER;
+        }
+        camera_flyto.set(selected.position.x, selected.position.y, selected.position.z + z_offset);
+    }
+}
+
+// Set visibility of stuff in a system
+function system_visibility(system, visibility) {
+    if(system.parent.name.startsWith("System/")) {
+        system = system.parent;
+    }
+    if(system.name.startsWith("System/")) {
+        for(var i = 0; i < system.children.length; i++) {
+            if(!system.children[i].name.startsWith("Sun/")) {
+                system.children[i].visible = visibility;
+            }
+        }
+    }
+}
+
+// Zoom to the clicked object
+function onDoubleClick(event) {
+    select_object(selected, true);
+    window.requestAnimationFrame(render);
+}
+
+// Handle window resises
 function onWindowResize() {
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -156,12 +246,34 @@ function onWindowResize() {
     window.requestAnimationFrame(render);
 }
 
+// Update the camera and render the scene
 function render() {
-    if(look_at != null) {
-        camera.position.setFromSphericalCoords(camera_range, camera_phi, camera_theta).add(look_at.position);
-        camera.lookAt(look_at.position);
-        console.log(camera_range, camera_phi, camera_theta, camera.position, look_at.position);
-        camera.updateProjectionMatrix();
+    var timer = false;
+    if(!camera.position.equals(camera_flyto)) {
+        var camera_distance = camera.position.distanceTo(camera_flyto);
+        move_toward(camera.position, camera_flyto, Math.max(TERAMETER, camera_distance / 10));
+        timer = true;
     }
+    if(!camera_lookat.equals(selected.position)) {
+        var camera_distance = camera.position.distanceTo(selected.position);
+        // distance adjusted lookat
+        camera_lookat.sub(camera.position).setLength(camera_distance).add(camera.position);
+        var lookat_distance = camera_lookat.distanceTo(selected.position);
+        move_toward(camera_lookat, selected.position, Math.max(TERAMETER / 100, lookat_distance / 10));
+        camera.lookAt(camera_lookat);
+        timer = true;
+    }
+    camera.updateProjectionMatrix();
     renderer.render(scene, camera);
+    if(timer) {
+        window.setTimeout(render, 10);
+    }
+}
+
+// Move a vector toward a point by a maximum amount
+function move_toward(position, toward, max) {
+    var offset = position.clone().sub(toward);
+    var distance = position.distanceTo(toward);
+    offset.setLength(Math.max(distance - max, 0));
+    position.copy(toward.clone().add(offset));
 }
