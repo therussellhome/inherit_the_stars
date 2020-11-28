5d3daf63ad379476625f75f5d12a07ce8816cf3d
Pam Russell <pam@karvonen.therussellhome.us>
2020-11-28 15:21:14 -0500

    started asteroid.py

---
+++ stars/asteroid.py
@@ -0,0 +1,32 @@
+from .defaults import Defaults
+from .location import Location
+from .reference import Reference
+import sys
+
+
+""" Default values (default, min, max)  """
+__defaults = {
+    'name': [''],
+    'minerals': [Minerals()],
+    'location': [Location()],
+    'mass': [10, 0, sys.maxsize],
+    'hyper': [1, 0, 20],
+    'target': [Location()],
+    'player': [Reference('Player')],
+    'decay_factor': [0.01, 0.0, 1.0],
+}
+    """decay = mass * hyper**2 * decay_factor, mass -= decay
+
+
+what does it need to have?
+minerals is class
+location is class
+mass
+speed
+target is location
+player (only if TANSTAAFL) is reference
+decay rate
+
+from what does it inherit
+buildable
+not minerals (it has minerals)
