4653740c4b3244c1af4482354ba78d741db41473
Tiernan Russell <tiernan@therussellhome.us>
2020-12-21 12:25:01 -0500

    getting planetary minister slider code

--- stars/ui/energy_minister.py
+++ stars/ui/energy_minister.py
@@ -10,6 +10,9 @@ __defaults = {
 class EnergyMinister(PlayerUI):
     def __init__(self, action, **kwargs):
         super().__init__(**kwargs)
+        if not self.player:
+            return
+        
 
 
 EnergyMinister.set_defaults(EnergyMinister, __defaults, sparse_json=False)
