fa4ad1d45f0b8a18867922639d55920bc4640a6e
Aryon Russell <aryon@therussellhome.us>
2023-09-01 12:03:44 -0400

    whatever I was working on last

--- stars/cost.py
+++ stars/cost.py
@@ -54,11 +54,11 @@ class Cost(Minerals):
         if self.energy > 0:
             html += '<i class="fa-bolt" title="Energy">' + str(self.energy) + '</i>'
         if self.titanium > 0:
-            html += '<i class="ti" title="Titanium">' + str(self.titanium) + '</i>'
+            html += '<i class="ti" title="Titanium">' + str(round(self.titanium)) + '</i>'
         if self.lithium > 0:
-            html += '<i class="li" title="Lithium">' + str(self.lithium) + '</i>'
+            html += '<i class="li" title="Lithium">' + str(round(self.lithium)) + '</i>'
         if self.silicon > 0:
-            html += '<i class="si" title="Silicon">' + str(self.silicon) + '</i>'
+            html += '<i class="si" title="Silicon">' + str(round(self.silicon)) + '</i>'
         return html
 
 
